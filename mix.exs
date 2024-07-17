a7ee1cb4c1c09fad96394abfdda98a58945a0654
Kenneth Mburu <mburunganga4@gmail.com>
2024-07-17 18:58:24 +0300

    Rename app to flex_web

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Flex.MixProject do
 
   def project do
     [
-      app: :flex,
+      app: :flex_web,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
